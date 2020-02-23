b9f63a8a00728707606df87bb9bdb0f0309efdad
Daniel Fanelli <daniel.r.fanellijr@gmail.com>
2020-02-23 13:58:31 -0800

    Update
    This is an update to final project of SecureDiary

--- App-Angular/ClientApp/src/app/app.module.ts
+++ App-Angular/ClientApp/src/app/app.module.ts
@@ -9,14 +9,22 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { CreateEntryComponent } from './create-entry/create-entry.component';
+import { EditEntryComponent } from './edit-entry/edit-entry.component';
+import { ReadEntryComponent } from './read-entry/read-entry.component';
+import { DeleteEntryComponent } from './delete-entry/delete-entry.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    // CounterComponent,
+    // FetchDataComponent,
+    CreateEntryComponent,
+    EditEntryComponent,
+    ReadEntryComponent,
+    DeleteEntryComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -24,8 +32,23 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      // < Template Component Routes >
+
+      // { path: 'counter', component: CounterComponent },
+      // { path: 'fetch-data', component: FetchDataComponent },
+
+      // < / Template Component Routes >
+
+      // < DearDiary Component Routes >
+
+      { path: 'create', component: CreateEntryComponent },
+      { path: 'edit', component: EditEntryComponent },
+      { path: 'read', component: ReadEntryComponent },
+      { path: 'delete', component: DeleteEntryComponent }
+
+
+      // < / DearDiary Component Routes >
+
     ])
   ],
   providers: [],
---
+++ App-Angular/ClientApp/src/app/entry.ts
@@ -0,0 +1,8 @@
+export class Entry {
+    constructor(
+        public id: number,
+        public chapter: string,
+        public dateCreated: Date,
+        public body: string
+    ) { }
+}
